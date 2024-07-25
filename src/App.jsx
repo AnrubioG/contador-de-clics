b94fbfc55e2132c26201d13ef8de3822d2572513
AnubioG <angela.rubio@genstudents.org>
2024-07-25 17:52:37 -0500

    Button component is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import "./App.css";
+import Button from "./components/Button";
+import fccLogo from "/img/freecodecamp-logo.png";
 
 function App() {
-  return <div className="App"></div>;
+  const manejarClic = () => {
+    console.log("Clic");
+  };
+
+  const reiniciarContador = () => {
+    console.log("Reiniciar");
+  };
+
+  return (
+    <div className="App">
+      <div className="fcc-logo-contenedor">
+        <img className="fcc-logo" src={fccLogo} alt="logo de freecodecamp" />
+      </div>
+
+      <div className="contenedor-principal">
+        <Button texto="Clic" esBotonDeClic={true} manejarClic={manejarClic} />
+        <Button
+          texto="Reiniciar"
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
